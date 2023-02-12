577376c94942bae8add45c0fcac047ffd4490ba3
Hubert Koniecko <hubert.koniecko@gmail.com>
2023-02-12 20:27:57 +0100

    add components to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import "./App.css";
+import { BrowserRouter} from "react-router-dom";
+import styled from "styled-components";
 
-import './App.css';
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
 
+const StyledAppWrapper = styled.div`
+  justify-self: center;
+  background-color: #bdcdd6;
+`;
 function App() {
   return (
-    <div className="App">
-      Czy działa?
-    </div>
+    <BrowserRouter>
+      <StyledAppWrapper>
+        <Header />
+        <NavBar />
+        <Content></Content>
+        <Footer />
+      </StyledAppWrapper>
+    </BrowserRouter>
   );
 }
 
